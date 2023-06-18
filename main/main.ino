b2ee39204514742f5a69998b100dfd6d177fa21a
GoshiX <90464815+GoshiX@users.noreply.github.com>
2023-06-18 15:22:15 +0300

    led blink(test)

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,10 @@
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(D5, D7); // RX, TX
 void setup() {
-  Serial.begin(115200);  // Скорость обмена данными с компьютером
-  Serial.println("Start!");
-  mySerial.begin(115200);  // Скорость обмена данными с GSM модулем
-  mySerial.println("AT");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+  digitalWrite(LED_BUILTIN, 0);
+  delay(300);
+  digitalWrite(LED_BUILTIN, 1);
+  delay(300);
 }
